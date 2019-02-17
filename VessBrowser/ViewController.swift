4e8f89aa227390b0bf5a96fa945a3294317f2752
Leo Liu <yliu@almondbank.com>
2019-02-17 00:59:21 -0500

    Implemented a simple browser

--- VessBrowser/ViewController.swift
+++ VessBrowser/ViewController.swift
@@ -9,13 +9,102 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class BrowserViewController: UIViewController {
 
 	@IBOutlet var webView: WKWebView!
+	@IBOutlet var progressBar: UIProgressView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		webView.load(URLRequest(url: URL(string: "https://www.google.com/")!))
+		webView.navigationDelegate = self
+		webView.allowsBackForwardNavigationGestures = true
+		webView.load(URLRequest(url: URL(string: "https://www.google.com/search?q=test")!))	// TODO
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.title), options: .new, context: nil)
+	}
+
+	deinit {
+		webView.removeObserver(self, forKeyPath: "estimatedProgress")
+		webView.removeObserver(self, forKeyPath: "title")
+	}
+
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		if keyPath == "estimatedProgress" {
+			print("PROGRESS", webView.estimatedProgress)
+			progressBar.progress = Float(webView.estimatedProgress)
+			if webView.estimatedProgress == 1 {
+				DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+					self.progressBar.progress = 0
+					self.progressBar.isHidden = true
+				}
+			} else {
+				progressBar.isHidden = false
+			}
+		} else if keyPath == "title" {
+			title = webView.title
+		}
+	}
+
+	func nextBrowser() -> BrowserViewController {
+		let storyboard = UIStoryboard(name: "Browser", bundle: nil)
+		if let browser = storyboard.instantiateInitialViewController() as? BrowserViewController {
+			return browser
+		}
+		fatalError("BROWSER failed initializing nextBrowser")
+	}
+
+	@IBAction func actionBack() {
+		webView.goBack()
+	}
+
+	@IBAction func actionForward() {
+		webView.goForward()
+	}
+}
+
+extension BrowserViewController: WKNavigationDelegate {
+	func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+		print("COMMIT")
+	}
+
+	func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+		print("START")
+	}
+
+	func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+		print("REDIRECTION")
+	}
+
+	/*
+	func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+		print("CHALLENGE")
+	}
+	*/
+
+	func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+		print("FAILED", error)
+	}
+
+	func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+		print("FAILED provisional nav", error)
+	}
+
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		print("FINISHED")
+		
+	}
+
+	func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+		print("TERMINATED")
+	}
+
+	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+		print("DECIDE action", navigationAction)
+		decisionHandler(.allow)
+	}
+
+	func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+		print("DECIDE response", navigationResponse)
+		decisionHandler(.allow)
 	}
 }
