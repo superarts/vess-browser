39f132f1134a360893a121840f41217bf7dafc24
Leo Liu <yliu@almondbank.com>
2019-02-17 23:25:41 -0500

    Added RxSwift & RxCocoa for WebsiteList

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '11.0'
+use_frameworks!
+
+target 'VessBrowser' do
+	pod 'RxSwift'
+	pod 'RxCocoa'
+end
--- VessBrowser/UI/ViewController/WebsiteListViewController.swift
+++ VessBrowser/UI/ViewController/WebsiteListViewController.swift
@@ -1,6 +1,58 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class WebsiteListViewController: UIViewController {
 	
 	@IBOutlet var tableView: UITableView!
+
+	var viewModel = WebsiteListViewModel()
+    private let disposeBag = DisposeBag()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		viewModel.setup()
+
+        viewModel.websites.asObservable()
+            .bind(to: tableView.rx.items(
+                cellIdentifier: "WebsiteListCell", cellType: UITableViewCell.self)) { (_, website, cell) in
+
+                cell.textLabel?.text = website.name
+                cell.detailTextLabel?.text = website.url.absoluteString
+            }
+            .disposed(by: disposeBag)
+
+        tableView.rx
+            .modelSelected(Website.self)
+            .subscribe(onNext: { website in
+                print("LIST selected", website)
+				//self.viewModel.nextClosure(topic)
+            })
+            .disposed(by: disposeBag)
+
+        tableView.rx
+            .itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                print("LIST tapped", indexPath.row)
+                self?.tableView.deselectRow(at: indexPath, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+struct Website {
+	let name: String
+	let url: URL
+}
+
+struct WebsiteListViewModel {
+    var websites = Variable<[Website]>([Website]())
+
+	func setup() {
+		let reddit = Website(
+			name: "Reddit",
+			url: URL(string: "https://www.reddit.com/")!
+		)
+		websites.value.append(reddit)
+	}
 }
