f22a306f682b8780127662e848b7fc4a38ef75be
Leo Liu <yliu@almondbank.com>
2019-03-02 22:32:12 -0500

    Refactoring: replaced XCTest with Quick and Nimble

--- VessBrowserTests/VessBrowserTests.swift
+++ VessBrowserTests/VessBrowserTests.swift
@@ -6,9 +6,38 @@
 //  Copyright © 2019 Super Art Software. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 @testable import VessBrowser
 
+class WebsiteListViewModelTests: QuickSpec, UnitTestDependencyInjectable {
+	override func spec() {
+		describe("WebsiteList") {
+			context("WebsiteListViewModel") {
+				it("is empty if it's not reloaded") {
+					let viewModel: WebsiteListViewModelProtocol = self.unitTestDependencyInjector.websiteListViewModel()
+					viewModel.sharedWebsiteAccessorDependencyInjector.registerEmpty()
+					expect(viewModel.websites.value).to(beEmpty())
+				}
+				it("is not empty if no websites loaded, as default websites will be used") {
+					let viewModel: WebsiteListViewModelProtocol = self.unitTestDependencyInjector.websiteListViewModel()
+					viewModel.sharedWebsiteAccessorDependencyInjector.registerEmpty()
+					viewModel.reload()
+					expect(viewModel.websites.value).toNot(beEmpty())
+				}
+				it("has 1 website if a single website is loaded") {
+					let viewModel: WebsiteListViewModelProtocol = self.unitTestDependencyInjector.websiteListViewModel()
+					viewModel.sharedWebsiteAccessorDependencyInjector.registerSingle()
+					viewModel.reload()
+					expect(viewModel.websites.value).to(haveCount(1))
+				}
+			}
+		}
+	}
+}
+
+/*
+import XCTest
 class WebsiteListViewModelTests: XCTestCase, UnitTestDependencyInjectable {
 
 	func testWebsiteListViewModelProtocolEmpty() {
@@ -25,3 +54,4 @@ class WebsiteListViewModelTests: XCTestCase, UnitTestDependencyInjectable {
 		XCTAssertEqual(viewModel.websites.value.count, 1)
 	}
 }
+*/
